a881070100c62bf562c390374b14a14a576f6a0b
Guy Ernest <guy.ernest@allcloud.io>
2019-07-13 20:53:52 +0300

    text change

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello World
+            Hello World!
           </p>
         </header>
       </div>
